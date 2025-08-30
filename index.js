1d33fdf9ac31292f3f2a158cafb870f6b9173ce5
bhgou <108895891+bhgou@users.noreply.github.com>
2025-08-31 02:33:26 +0400

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = "8078943288:AAGbDX6HcmZ3NaYejKMVAtPXO_NieDfOvgQ"
+const web_app = {url: "https://ya.ru"}
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', async(msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Вход в приложение', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Войти',web_app}]
+        ]
+      }
+    });
+  }
+});
